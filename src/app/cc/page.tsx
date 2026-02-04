113d01190545acf7099a3fbea9b2217dd8c7fa41
“izawasoma” <izawasoma@gmail.com>
2026-02-04 22:49:45 +0900

    サーバーコンポーネントとクライアントコンポーネントの違い／editorconfigを追加（おまけ）

---
+++ src/app/cc/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+const ClientComponent = () => {
+  console.log("This is a client component");
+  return <div>ClientComponent</div>;
+};
+
+export default ClientComponent;
---
+++ src/app/sc/page.tsx
@@ -0,0 +1,6 @@
+const ServerComponent = () => {
+  console.log('This is a server component');
+  return <div>ServerComponent</div>;
+};
+
+export default ServerComponent;
